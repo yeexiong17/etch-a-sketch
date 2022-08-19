f0eff59b6b918fb78f63c3569929fdc29650afd2
yeexiong17 <wyeexiong17@gmail.com>
2022-08-19 22:42:49 +0800

    Added background color to grid when mouseover, added prompt user when button is clicked

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 
+function makeNewGrid(numRow, numCol){
+
+}
+
 for(let i = 0; i < 256; i++){
 
     const select1 = document.querySelector(".container");
 
     const div1 = document.createElement("div");
-    div1.classList.add('gridbox');
+    div1.classList.add('gridBox');
 
     select1.appendChild(div1);
+
+    div1.addEventListener('mouseover', function() {
+        div1.setAttribute('style', 'background-color: blue;');
+    });
     
 }
+
+const btn1 = document.querySelector(".popup-btn");
+
+btn1.addEventListener('click', function(){
+
+    let numRow;  
+    let numCol; 
+
+    numRowl = prompt("Enter number of row");
+    numCol = prompt("Enter number of column");
+
+    makeNewGrid(numRow, numCol);
+
+});
