5b2dd184c63ebc069d2f2101bf03c1b49966c561
yeexiong17 <wyeexiong17@gmail.com>
2022-08-19 21:55:27 +0800

    used DOM to make 256 divs for grid

---
+++ script.js
@@ -0,0 +1,11 @@
+
+for(let i = 0; i < 256; i++){
+
+    const select1 = document.querySelector(".container");
+
+    const div1 = document.createElement("div");
+    div1.classList.add('gridbox');
+
+    select1.appendChild(div1);
+    
+}
