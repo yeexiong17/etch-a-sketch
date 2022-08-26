aba517dcf5dd64c3f0ab87163bdecc34872e4108
yeexiong17 <wyeexiong17@gmail.com>
2022-08-26 11:50:40 +0800

    Added background color. Customised slider. Done click and drag or click only function

--- script.js
+++ script.js
@@ -4,6 +4,40 @@ var colOut = document.getElementById("col");
 var select1 = document.querySelector(".container");
 var btn1 = document.getElementById("apply");
 
+// Event Listener & paintGrid are made based on Pau Rodriguez Molina's (Odin Project) code.
+// Paint only when mouse being clicked / clicked and drag.
+select1.addEventListener('mousedown', event =>{ 
+    
+    selectedColor = "blue";
+
+    paintGridEvent = paintGrid(event, selectedColor);
+
+    if(event.buttons == 1){        
+        window.addEventListener('mouseover', (e) => {
+            if(selectedColor == 'blue'){
+                paintGrid(e, selectedColor);
+            }            
+        });
+    }
+
+});
+
+function paintGrid(element, color){ 
+
+    if(element.buttons == 1){
+
+        if(element.target.classList == 'gridBox'){
+            let box = element.target;    
+             box.style.backgroundColor = color;
+        }  
+    }else{
+        //Exit condition if mouse is not clicked.
+        return;
+    }
+
+}
+
+// Default grid of 16 x 16
 for(let i = 0; i < 256; i++){
 
     select1.setAttribute('style', 'grid-template-columns: repeat(16, auto);');
@@ -12,15 +46,12 @@ for(let i = 0; i < 256; i++){
     div1.classList.add('gridBox');
 
     select1.appendChild(div1);
-
-    div1.addEventListener('mousedown', function() {
-        div1.setAttribute('style', 'background-color: blue;');
-    });
-    
 }
 
+// Print 16 x 16 beside slider
 colOut.innerHTML = newCol.value + " x " + newCol.value;
 
+// Print new slider value (eg: 5 x 5) when dragged
 newCol.oninput = function() {
   colOut.innerHTML = this.value + " x " + this.value;
 }
@@ -44,10 +75,6 @@ btn1.addEventListener('click', function(){
         select1.style.gridTemplateColumns = `repeat(${newCol.value}, auto)`;
 
         select1.appendChild(div2);
-
-        div2.addEventListener('click', function() {
-            div2.setAttribute('style', 'background-color: blue;');
-        });
 
     }
 
