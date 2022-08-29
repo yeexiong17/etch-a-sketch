94df108c762d5a293c1843f6c014909b134be674
yeexiong17 <wyeexiong17@gmail.com>
2022-08-29 16:57:56 +0800

    Added rainbow function for painting the grid. Adjusted the code for buttons and control layout.

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ var colOut = document.getElementById("col");
 var select1 = document.querySelector(".container");
 var btn1 = document.getElementById("apply");
 var black = document.querySelector(".btn-1");
+var rainbow = document.querySelector(".btn-2");
 var erase = document.querySelector(".btn-3");
 var clear = document.querySelector(".btn-4");
 
 let state_1;
 let state_2;
-let state_3;  
+let state_3;
+let state_4;  
 
-function state_of_button(state1, state2, state3){
+function state_of_button(state1, state2, state3, state4){
 
-    if(state1 == 1 && state2 == 0 && state3 == 0){
+    if(state1 == 1 && state2 == 0 && state3 == 0 && state4 == 0){
         black.style.backgroundColor = `black`;
         black.style.color = `white`;
     }else{
@@ -21,7 +23,15 @@ function state_of_button(state1, state2, state3){
         black.style.color = `black`;
     }
 
-    if(state1 == 0 && state2 == 1 && state3 == 0){
+    if(state1 == 0 && state2 == 1 && state3 == 0 && state4 == 0){
+        rainbow.style.backgroundColor = `black`;
+        rainbow.style.color = `white`;
+    }else{
+        rainbow.style.backgroundColor = `white`;
+        rainbow.style.color = `black`;
+    }
+
+    if(state1 == 0 && state2 == 0 && state3 == 1 && state4 == 0){
         erase.style.backgroundColor = `black`;
         erase.style.color = `white`;
     }else{
@@ -29,7 +39,7 @@ function state_of_button(state1, state2, state3){
         erase.style.color = `black`;
     }
 
-    if(state1 == 0 && state2 == 0 && state3 == 1){
+    if(state1 == 0 && state2 == 0 && state3 == 0 && state4 == 1){
         clear.style.backgroundColor = `black`;
         clear.style.color = `white`;
     }else{
@@ -37,22 +47,40 @@ function state_of_button(state1, state2, state3){
         clear.style.color = `black`;
     }
 
-
 }
 
+//For painting grid
 function paintGrid(element, color){ 
 
-    if(element.buttons == 1){
-
-        if(element.target.classList == 'gridBox'){
-            let box1 = element.target;    
-                box1.style.backgroundColor = color;
-        }  
+    if(color !== "rainbow"){
+        if(element.buttons == 1){
+            if(element.target.classList == 'gridBox'){
+                let box1 = element.target;    
+                    box1.style.backgroundColor = color;
+            }  
+        }else{
+            //Exit condition if mouse is not clicked.
+            return;
+        }
+    
+    // If color is equal to rainbow
     }else{
-        //Exit condition if mouse is not clicked.
-        return;
-    }
 
+        if(element.buttons == 1){
+            if(element.target.classList == 'gridBox'){
+                let box2 = element.target;
+                    box2.style.backgroundColor = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+            }
+        }else{
+            return;
+        }
+
+    }
+}
+
+// Generate random number between 0 - 255
+function random(number) {
+    return Math.floor(Math.random() * (number+1));
 }
 
 // Default grid of 16 x 16
@@ -66,7 +94,7 @@ for(let i = 0; i < 256; i++){
     select1.appendChild(div1);
 }
 
-// Print 16 x 16 beside slider
+// Print 16 x 16 below slider
 colOut.innerHTML = newCol.value + " x " + newCol.value;
 
 // Print new slider value (eg: 5 x 5) when dragged
@@ -105,8 +133,9 @@ black.addEventListener('click', function(){
     state_1 = 1;
     state_2 = 0;
     state_3 = 0;
+    state_4 = 0;
 
-    state_of_button(state_1, state_2, state_3);
+    state_of_button(state_1, state_2, state_3, state_4);
 
     select1.addEventListener('mousedown', event =>{ 
         
@@ -125,13 +154,41 @@ black.addEventListener('click', function(){
     });
 });
 
-erase.addEventListener('click', function(){
+rainbow.addEventListener('click', function(){
 
     state_1 = 0;
     state_2 = 1;
     state_3 = 0;
+    state_4 = 0;
+
+    state_of_button(state_1, state_2, state_3, state_4);
+
+    select1.addEventListener('mousedown', event => {
+
+        selectedColor = "rainbow";
+
+        gridBoxEvent = paintGrid(event, selectedColor);
+
+        if(event.buttons == 1){
+
+            window.addEventListener('mouseover', (e) => {
+                if(selectedColor == "rainbow"){
+                    gridBoxEvent = paintGrid(e, selectedColor);
+                }
+            });
+        }
+    });
+});
+
+// Erase grid box by click or click and drag around the grid box
+erase.addEventListener('click', function(){
+
+    state_1 = 0;
+    state_2 = 0;
+    state_3 = 1;
+    state_4 = 0;
     
-    state_of_button(state_1, state_2, state_3);
+    state_of_button(state_1, state_2, state_3, state_4);
 
     select1.addEventListener('mousedown', event => {
         selectedColor = "white";
@@ -154,9 +211,10 @@ clear.addEventListener('click', function(){
 
     state_1 = 0;
     state_2 = 0;
-    state_3 = 1;
+    state_3 = 0;
+    state_4 = 1;
 
-    state_of_button(state_1, state_2, state_3);    
+    state_of_button(state_1, state_2, state_3, state_4);    
 
     var gridBox = document.querySelectorAll(".gridBox");
     
